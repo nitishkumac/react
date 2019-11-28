d45ba2faee3fa246d2926b0e9cf83bb4f9d22e1c
nitishkumac <38972698+nitishkumac@users.noreply.github.com>
2019-11-28 15:35:18 +0530

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+ 
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
+
+class Main extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+      hits: [],
+    };
+  }
+
+componentDidMount() {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
+  }
+
+  render() {
+      const { hits } = this.state;
+    return (
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+          <ul>
+        {hits.map(hit =>
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        )}
+      </ul>
+        </div>
+      </HashRouter>
+       
+      
+
+    );
+  }
+}
+ 
+export default Main;
